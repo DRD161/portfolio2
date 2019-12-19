43d82d1a0128f591f562ec7066f005e5c2c3d2f3
Dylan <dylan@dylandavenport.com>
2019-12-19 08:15:24 -0500

    Done with SideNav. Working on Homepage

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -2,16 +2,40 @@ import { Link } from "gatsby"
 import React from "react"
 
 import { slide as Menu } from "react-burger-menu"
+import { IconContext } from "react-icons"
+import { FaRegEnvelope, FaLinkedin, FaGithubSquare } from "react-icons/fa"
 
 import "../styles/sidenav.scss"
+
 const SideNav = () => (
   <header>
     <div>
       <Menu crossButtonClassName={"cross-hover"}>
-        <Link to="/">home</Link>
-        <Link to="/about">about</Link>
-        <Link to="/projects">projects</Link>
-        <Link to="/contact">contact</Link>
+        <Link to="/" activeClassName="active">
+          home
+        </Link>
+        <Link to="/about" activeClassName="active">
+          about
+        </Link>
+        <Link to="/projects" activeClassName="active">
+          projects
+        </Link>
+        <Link to="/contact" activeClassName="active">
+          contact
+        </Link>
+        <div className="icons-wrapper">
+          <IconContext.Provider value={{ className: "social-icons" }}>
+            <Link to="/">
+              <FaRegEnvelope />
+            </Link>
+            <Link to="/">
+              <FaLinkedin />
+            </Link>
+            <Link to="/">
+              <FaGithubSquare />
+            </Link>
+          </IconContext.Provider>
+        </div>
       </Menu>
     </div>
   </header>
--- src/components/layout.js
+++ src/components/layout.js
@@ -6,7 +6,7 @@
  */
 
 import React from "react"
-import PropTypes from "prop-types"
+// import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import SideNav from "./SideNav"
