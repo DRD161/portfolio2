bf537876b27a8d3bc70d364725d508f40498919d
DRD161 <dylan@dylandavenport.com>
2019-12-20 11:33:12 -0500

    Adding text to homepage component.

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -2,32 +2,29 @@ import { Link } from "gatsby"
 import React from "react"
 
 import { slide as Menu } from "react-burger-menu"
-import { IconContext } from "react-icons"
-import SocialIcons from "../components/SocialIcons"
+import NavSocialIcons from "./NavSocialIcons"
 
 import "../styles/sidenav.scss"
 
 const SideNav = () => (
   <header>
-    <div>
-      <Menu crossButtonClassName={"cross"}>
-        <Link to="/" activeClassName="active">
-          home
-        </Link>
-        <Link to="/about" activeClassName="active">
-          about
-        </Link>
-        <Link to="/projects" activeClassName="active">
-          projects
-        </Link>
-        <Link to="/contact" activeClassName="active">
-          contact
-        </Link>
-        <IconContext.Provider value={{ className: "sidenav-icons" }}>
-          <SocialIcons className="icons-wrapper" />
-        </IconContext.Provider>
-      </Menu>
-    </div>
+    <Menu crossButtonClassName={"cross"}>
+      <Link to="/" activeClassName="active">
+        home
+      </Link>
+      <Link to="/about" activeClassName="active">
+        about
+      </Link>
+      <Link to="/projects" activeClassName="active">
+        projects
+      </Link>
+      <Link to="/contact" activeClassName="active">
+        contact
+      </Link>
+      <div className="sidenav-icons-wrapper">
+        <NavSocialIcons />
+      </div>
+    </Menu>
   </header>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,19 @@
 import React from "react"
 import Layout from "../components/layout"
-// import { Link } from "gatsby"
 import { IconContext } from "react-icons"
 import SEO from "../components/seo"
 import TopBlob from "../components/TopBlob"
 import BottomBlob from "../components/BottomBlob"
-import HomeTextBack from "../components/HomeTextBack"
-import HomeTextFront from "../components/HomeTextFront"
-import SocialIcons from "../components/SocialIcons"
+import HomeHeader from "../components/HomeHeader"
+import HomeSubText from "../components/HomeSubText"
+import SocialIcons from "../components/NavSocialIcons"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <TopBlob />
-    <HomeTextBack />
-    <HomeTextFront className="foreground-text" />
+    <HomeHeader />
+    <HomeSubText />
     <BottomBlob />
     <IconContext.Provider value={{ className: "homepage-icons" }}>
       <SocialIcons />
