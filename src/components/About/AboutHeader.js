575b80c53982194c953739cf180d99a96668a1fb
Dylan <dylan@dylandavenport.com>
2020-01-23 01:21:25 -0500

    :Playing with animations.

--- src/components/About/AboutHeader.js
+++ src/components/About/AboutHeader.js
@@ -1,5 +1,10 @@
-import React from "react"
+import React, { useEffect } from "react"
+import anime from "animejs"
 import styled from "styled-components"
+
+const Wrapper = styled.div`
+  position: relative;
+`
 
 const AboutHeaderText = styled.h3`
   font-size: 6rem;
@@ -13,16 +18,37 @@ const RedBar = styled.div`
   background: #d84358;
   position: absolute;
   top: 16%;
-  right: 100%;
+  right: 90%;
   height: 70%;
   margin-right: 5px;
 `
+const tl = anime.timeline({
+  easing: "easingExpo",
+  duration: 1000,
+})
 
-const AboutHeader = () => (
-  <AboutHeaderText>
-    <RedBar />
-    about
-  </AboutHeaderText>
-)
+export default function AboutHeader() {
+  useEffect(() => {
+    const tl = anime.timeline({
+      duration: 1000,
+      easing: "easeInOutBack",
+    })
+    tl.add({
+      targets: ".bar",
+      opacity: [0, 100],
+      top: ["-20%", "16%"],
+    }).add({
+      targets: ".about-header",
+      delay: 200,
+      opacity: [0, 100],
+      left: ["8.6%", "10%"],
+    })
+  })
 
-export default AboutHeader
+  return (
+    <Wrapper>
+      <RedBar className="bar" />
+      <AboutHeaderText className="about-header">about</AboutHeaderText>
+    </Wrapper>
+  )
+}
--- src/components/About/AboutSubText.js
+++ src/components/About/AboutSubText.js
@@ -1,9 +1,10 @@
-import React from "react"
+import React, { useEffect } from "react"
+import anime from "animejs"
 import styled from "styled-components"
 import Skills from "../About/Skills"
 
 const SubText = styled.p`
-  font-size: 1.6rem;
+  font-size: 2.3rem;
   color: #eeeeee;
 `
 
@@ -15,22 +16,41 @@ const EmailLink = styled.a`
   }
 `
 
-const AboutSubText = () => (
-  <section>
-    <SubText>Front End Developer, husband, father & coffee lover.</SubText>
-    <SubText>
-      I specialize in creating user-friendly interfaces using semantic markup
-      and JavaScript in order to make the web a better place.
-    </SubText>
-    <SubText>
-      I'm always open to collaborate with great people and solve unique
-      challenges. If you're interested <br></br>
-      <EmailLink href="mailto:dylan@dylandavenport.com">
-        let's get to work!
-      </EmailLink>
-    </SubText>
-    <Skills />
-  </section>
-)
+export default function AboutSubText() {
+  useEffect(() => {
+    const tl = anime
+      .timeline({
+        duration: 1000,
+        easing: "easeInOutBack",
+      })
+      .add({
+        targets: ".about-text",
+        delay: anime.stagger(500),
+        top: ["-20%", 0],
+        opacity: [0, 100],
+      })
+  })
 
-export default AboutSubText
+  return (
+    <section className="subtext-wrapper">
+      <SubText className="about-text">
+        Front End Developer, husband, father & coffee lover.
+      </SubText>
+      <SubText className="about-text">
+        I specialize in creating user-friendly interfaces using semantic markup
+        and JavaScript in order to make the web a better place.
+      </SubText>
+      <SubText className="about-text">
+        I'm always open to collaborate with great people and solve unique
+        challenges. If you're interested <br></br>
+        <EmailLink
+          href="mailto:dylan@dylandavenport.com"
+          className="about-text"
+        >
+          let's get to work!
+        </EmailLink>
+      </SubText>
+      <Skills />
+    </section>
+  )
+}
